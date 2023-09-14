167ba3b59c13cacc5073d0499f3d62ba012dcf54
Geoffery Powell <powellgeoffery@gmail.com>
2023-09-14 14:39:54 -0500

    Made bat more flexible

--- get_usernames.bat
+++ get_usernames.bat
@@ -1,6 +1,16 @@
 @echo off
 setlocal
 set "VRCPATH=%USERPROFILE%\AppData\LocalLow\VRChat\vrchat"
-python "%VRCPATH%\get_usernames.py"
+if exist "%VRCPATH%\get_usernames.py" (
+    python "%VRCPATH%\get_usernames.py"
+) else (
+    echo "get_usernames.py not found in %VRCPATH%"
+    if exist ".\get_usernames.py" (
+        echo "get_usernames.py found in the current directory"
+        python ".\get_usernames.py"
+    ) else (
+        echo "get_usernames.py not found in the current directory"
+    )
+)
 endlocal
 pause
